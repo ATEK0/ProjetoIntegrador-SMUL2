4e75dc15ee3e68927a80287f860a1d19669a2b05
GFreitas3 <goncaloafreitas86@gmail.com>
2026-04-17 14:21:07 +0100

    US_A004: - Criação do projeto front-end (.NET)

---
+++ PortalFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortalFrontend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
