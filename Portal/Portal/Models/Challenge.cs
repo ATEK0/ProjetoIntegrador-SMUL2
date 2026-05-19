472012158b28fba3f7d04468a70a6e62f4d04f55
ATEK0 <videogamer.neves@gmail.com>
2026-05-19 18:24:41 +0100

    small changes to try and use containers

--- Portal/Portal/Models/Challenge.cs
+++ Portal/Portal/Models/Challenge.cs
@@ -11,6 +11,7 @@ namespace Portal.Models
         private string _description;
         private string _accessLinkCode;
 
+
         public int TeacherId
         {
             get { return _teacherId; }
